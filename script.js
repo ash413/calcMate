1b384eb901648665f9ac2aba53f1b38c93d97f9e
Vaishnavi Kadam <vaishnavi.kadam413@gmail.com>
2024-07-13 21:34:05 +0530

    js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-document.addEventListener()
+document.addEventListener("DOMConentLoaded", function(){
 
+    
+    const display = document.getElementById('display');
+    const buttons = document.querySelectorAll('.btn');
+    let displayValue = '';
 
-function calculate(firstOperand, operator, secondOperand){
-    const a = parseFloat(firstOperand);
-    const b = parseFloat(secondOperand);
-
-    switch(operator){
+    function calculate(firstOperand, operator, secondOperand){
+        const a = parseFloat(firstOperand);
+        const b = parseFloat(secondOperand);
+        
+        switch(operator){
         case '+':
             return a + b;
         case '-':
@@ -13,8 +17,14 @@ function calculate(firstOperand, operator, secondOperand){
         case '*':
             return a * b;
         case '/':
-            return a / b;
+            return b===0 ? 'Error' : a / b;
         default:
             return '';
+        }
     }
-}
+    
+    document.querySelector('#clear').addEventListener("click", function(){
+
+    })
+
+})
